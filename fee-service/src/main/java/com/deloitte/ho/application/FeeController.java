ce968c5b48929997abff8f534ec511f82d66aea5
Singh, Shiv (UK - London) <shivxsingh@deloitte.co.uk>
2017-04-20 14:56:24 +0100

    Initial Commit

---
+++ fee-service/src/main/java/com/deloitte/ho/application/FeeController.java
@@ -0,0 +1,28 @@
+package com.deloitte.ho.application;
+
+import com.deloitte.ho.application.model.Fee;
+import com.deloitte.ho.application.service.FeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by akothiyal on 19/04/2017.
+ */
+@RestController
+public class FeeController {
+
+
+    FeeService feeService;
+
+    public FeeController( FeeService feeService) {
+        this.feeService = feeService;
+    }
+
+    @RequestMapping(path = "/fees", method =  RequestMethod.GET)
+    public Fee getFees(){
+        return feeService.getFees();
+    }
+
+}
